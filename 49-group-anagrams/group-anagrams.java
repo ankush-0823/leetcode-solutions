68f0e845a8b742be84550da3c7c9a368df6c7784
Ankush Kumar <121677364+ankush-0823@users.noreply.github.com>
2024-02-06 23:32:27 +0530

    Time: 6 ms (92.74%) | Memory: 47.5 MB (65.51%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,26 +1,25 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-   int n=strs.length;
    HashMap<String,List<String>> h=new HashMap<>();
-   for(int i=0;i<n;i++){
-       String s=strs[i];
-       char[] c=s.toCharArray();
-       Arrays.sort(c);
-       String temp=new String(c);
-       if(h.containsKey(temp)){
-           List<String> list=h.get(temp);
-           list.add(s);
-           h.put(temp,list);
-       }
-       else{
-         List<String> list=new ArrayList<>();
-         list.add(s);
-         h.put(temp,list);  
-       }
+        int n=strs.length;
+    for(int i=0;i<n;i++){
+    String s=strs[i];
+    char[] c=s.toCharArray();
+        Arrays.sort(c);
+       String temp = new String(c);
+     if(h.containsKey(temp)){
+    List<String> list=h.get(temp);
+       list.add(s);  
+    h.put(temp,list);
+     }   
+     else{
+      List<String> list=new ArrayList<>();
+       list.add(s);
+         h.put(temp,list);
         }
-       List<List<String>> ans=new ArrayList<>();   
-       ans.addAll(h.values());
-       return ans;  
-       }
-   }
-    
+    }
+    List<List<String>> ans=new ArrayList<>();
+        ans.addAll(h.values());
+        return ans;
+      }     
+    }
