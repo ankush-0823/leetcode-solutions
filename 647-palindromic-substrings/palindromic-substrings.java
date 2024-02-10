9bb4872a1a32568247c3a6784331cabf3d8abd70
Ankush Kumar <121677364+ankush-0823@users.noreply.github.com>
2024-02-10 13:38:49 +0530

    Time: 2 ms (96.29%) | Memory: 41.2 MB (86.32%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,27 +1,27 @@
 class Solution {
+    private int count = 0;
+
+    private void check(String s, int i, int j, int n) {
+        while (i >= 0 && j < n && s.charAt(i) == s.charAt(j)) {
+            count++;
+            i--; // expanding from center
+            j++; // expanding from center
+        }
+    }
+
     public int countSubstrings(String s) {
         int n = s.length();
-        boolean[][] t = new boolean[n][n];
+        count = 0;
 
-        int count = 0;
-        for (int L = 1; L <= n; L++) {
-            for (int i = 0; i + L <= n; i++) {
-                int j = i + L - 1;
-
-                if (i == j) {
-                    t[i][i] = true; // Single characters are palindrome
-                } else if (i + 1 == j) {
-                    t[i][j] = (s.charAt(i) == s.charAt(j)); // Strings of 2 Length
-                } else {
-                    t[i][j] = (s.charAt(i) == s.charAt(j) && t[i + 1][j - 1]);
-                }
-
-                if (t[i][j]) {
-                    count++;
-                }
-            }
+        /*
+         * Every single character in the string is a center for possible odd-length
+         * palindromes: check(s, i, i); Every pair of consecutive characters in the
+         * string is a center for possible even-length palindromes: check(s, i, i+1);
+         */
+        for (int i = 0; i < n; i++) {
+            check(s, i, i, n);
+            check(s, i, i + 1, n);
         }
-
         return count;
     }
 }
