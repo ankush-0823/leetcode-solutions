48d1face60b2b19a523eed121fb449ae639a9895
Ankush Kumar <121677364+ankush-0823@users.noreply.github.com>
2024-03-01 00:01:32 +0530

    Time: 10 ms (87.30%) | Memory: 66.3 MB (32.62%) - LeetSync

--- 1731-even-odd-tree/even-odd-tree.java
+++ 1731-even-odd-tree/even-odd-tree.java
@@ -13,30 +13,26 @@
  *     }
  * }
  */
-import java.util.*;
-
 class Solution {
     public boolean isEvenOddTree(TreeNode root) {
         Queue<TreeNode> queue = new LinkedList<>();
-        
         queue.offer(root);
         boolean evenLevel = true;
-        
+
         while (!queue.isEmpty()) {
-            int n = queue.size();
+            int size = queue.size();
             int prev = evenLevel ? Integer.MIN_VALUE : Integer.MAX_VALUE;
-            
-            while (n-- > 0) {
+
+            while (size-- > 0) {
                 TreeNode node = queue.poll();
-                
-                if (evenLevel && (node.val % 2 == 0 || node.val <= prev)) {
+
+                if ((evenLevel && (node.val % 2 == 0 || node.val <= prev)) ||
+                    (!evenLevel && (node.val % 2 == 1 || node.val >= prev))) {
                     return false;
                 }
-                if (!evenLevel && (node.val % 2 == 1 || node.val >= prev)) {
-                    return false;
-                }
-                
+
                 prev = node.val;
+
                 if (node.left != null) {
                     queue.offer(node.left);
                 }
@@ -44,10 +40,12 @@ class Solution {
                     queue.offer(node.right);
                 }
             }
-            
+
             evenLevel = !evenLevel;
         }
-        
+
         return true;
     }
 }
+
+
