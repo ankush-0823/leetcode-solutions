f4e906f5d3824eb651e7844e0ee674fdc99b2603
Ankush Kumar <121677364+ankush-0823@users.noreply.github.com>
2024-03-14 20:40:08 +0530

    Time: 3 ms (69.27%) | Memory: 48.1 MB (43.72%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -1,22 +1,29 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-        int n = nums.length;
-        
-        // HashMap to store cumulative sums and their frequencies
-        HashMap<Integer, Integer> mp = new HashMap<>();
+        int prefixZeros = 0;
+        int windowSum = 0;
         int count = 0;
-        int currSum = 0;
-        mp.put(0, 1);
-        for (int num : nums) {
-            currSum += num;
-
-            // If there is a previous sum such that (current sum - previous sum) equals goal, increment count by the frequency of that previous sum
-            if (mp.containsKey(currSum - goal)) {
-                count += mp.get(currSum - goal);
+        
+        int i = 0, j = 0;
+        
+        while (j < nums.length) {
+            windowSum += nums[j];
+            
+            while (i < j && (nums[i] == 0 || windowSum > goal)) {
+                if (nums[i] == 1) {
+                    prefixZeros = 0;
+                } else {
+                    prefixZeros += 1;
+                }
+                
+                windowSum -= nums[i];
+                i++;
             }
             
-            // Increment the frequency of current sum in the map
-            mp.put(currSum, mp.getOrDefault(currSum, 0) + 1);
+            if (windowSum == goal) {
+                count += 1 + prefixZeros;
+            }
+            j++;
         }
         
         return count;
