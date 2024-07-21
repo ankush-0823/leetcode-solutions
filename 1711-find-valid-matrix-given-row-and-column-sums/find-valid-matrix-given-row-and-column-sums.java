ca1959153c311bb5f4c07bc8de279b25f39d7771
Ankush Kumar <121677364+ankush-0823@users.noreply.github.com>
2024-07-21 08:41:04 +0530

    Time: 1 ms (100.00%) | Memory: 52.8 MB (53.17%) - LeetSync

--- 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.java
+++ 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.java
@@ -1,20 +1,34 @@
 class Solution {
     public int[][] restoreMatrix(int[] rowSum, int[] colSum) {
-        int m = rowSum.length;
-        int n = colSum.length;
-        int[][] matrix = new int[m][n];
-        
-        int i = 0, j = 0;
-        while (i < m && j < n) {
-            matrix[i][j] = Math.min(rowSum[i], colSum[j]);
+        int rows = rowSum.length;
+        int cols = colSum.length;
+        int cur_row = 0, cur_col = 0;
+        int[][] res = new int[rows][cols];
 
-            rowSum[i] -= matrix[i][j];
-            colSum[j] -= matrix[i][j];
+        while (cur_row < rows || cur_col < cols) {
+            if (cur_row >= rows) {
+                res[rows - 1][cur_col] = colSum[cur_col];
+                cur_col++;
+                continue;
+            } else if (cur_col >= cols) {
+                res[cur_row][cols - 1] = rowSum[cur_row];
+                cur_row++;
+                continue;
+            }
 
-            if (rowSum[i] == 0) i++;
-            if (colSum[j] == 0) j++;
+            int value_to_put = Math.min(rowSum[cur_row], colSum[cur_col]);
+            rowSum[cur_row] -= value_to_put;
+            colSum[cur_col] -= value_to_put;
+            res[cur_row][cur_col] = value_to_put;
+
+            // I write this as this because it's possible that rowSum[cur_row] == colSum[cur_col] and we'll want to move both row and col pointers
+            if (rowSum[cur_row] == 0) {
+                cur_row++;
+            }
+            if (colSum[cur_col] == 0) {
+                cur_col++;
+            }
         }
-        
-        return matrix;
+        return res;
     }
 }
