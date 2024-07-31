10611fb8a6f9905034ae7641e6a34dd69fda932c
Ankush Kumar <121677364+ankush-0823@users.noreply.github.com>
2024-07-31 19:50:35 +0530

    Time: 3 ms (95.39%) | Memory: 48.9 MB (83.36%) - LeetSync

--- 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.java
+++ 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.java
@@ -1,26 +1,26 @@
 class Solution {
-    public int maxSubarray(int[] nums, int n) {
-        int res = nums[0];
-        int maxEnd = nums[0];
-        for (int i = 1; i < n; i++) {
-            maxEnd = Math.max(maxEnd + nums[i], nums[i]);
-            res = Math.max(maxEnd, res);
+     public int maxSubarraySumCircular(int[] nums) {
+        int n=nums.length;
+        int normalsum = maxNormal(nums,n);
+        if(normalsum<0) return normalsum;
+         int arrsum=0;
+       for(int i=0;i<n;i++){
+         arrsum+= nums[i];
+        nums[i]= -nums[i];
+       }
+       int maxcircularsum = arrsum + maxNormal(nums,n);
+       int res = Math.max(maxcircularsum , normalsum);
+       return res;
+        
+    }
+    public int maxNormal(int arr[] , int n){
+        int res= arr[0];
+        int currsum= arr[0];
+
+        for(int i=1;i<n;i++){
+            currsum= Math.max(currsum+arr[i], arr[i]);
+            res = Math.max(res , currsum);
         }
         return res;
-    }
-
-    public int maxSubarraySumCircular(int[] nums) {
-        int n = nums.length;
-        int maxNorm = maxSubarray(nums, n);
-        if (maxNorm < 0) return maxNorm;
-
-        int curr_sum = 0;
-        for (int i = 0; i < n; i++) {
-            curr_sum += nums[i];
-            nums[i] = -nums[i];
-        }
-        
-        int maxCircular = curr_sum + maxSubarray(nums, n);
-        return Math.max(maxCircular, maxNorm);
     }
 }
