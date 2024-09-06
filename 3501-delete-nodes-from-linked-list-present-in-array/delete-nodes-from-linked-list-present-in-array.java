dfb3c7df1930cd67786aa9362b15323b0ac2879b
Ankush Kumar <121677364+ankush-0823@users.noreply.github.com>
2024-09-06 09:12:32 +0530

    Time: 5 ms (99.28%) | Memory: 70.7 MB (16.71%) - LeetSync

--- 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.java
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.java
@@ -8,6 +8,7 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
 class Solution {
     public ListNode modifiedList(int[] nums, ListNode head) {
         int max = 0;
