fddc4a78ba3c2dbc5bfbfe8160fdaaba6541cd22
Ankush Kumar <121677364+ankush-0823@users.noreply.github.com>
2024-09-09 10:52:16 +0530

    Time: 4 ms (100.00%) | Memory: 56.1 MB (98.93%) - LeetSync

--- 2411-spiral-matrix-iv/spiral-matrix-iv.java
+++ 2411-spiral-matrix-iv/spiral-matrix-iv.java
@@ -8,16 +8,6 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public int[][] spiralMatrix(int rows, int columns, ListNode head) {
         int[][] matrix = new int[rows][];
