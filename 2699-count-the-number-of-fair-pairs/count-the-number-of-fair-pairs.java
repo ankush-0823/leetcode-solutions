28a03394b3d073d87ecf05769edfdb12994febe4
Ankush Kumar <ank86253@gmail.com>
2025-04-19 23:38:09 +0530

    Time: 64 ms (45.27%) | Memory: 57.7 MB (21.34%) - LeetSync

--- 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.java
+++ 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.java
@@ -1,17 +1,43 @@
 class Solution {
-  public long countFairPairs(int[] nums, int lower, int upper) {
-   
-    Arrays.sort(nums);
-    return countLess(nums, upper) - countLess(nums, lower - 1);
-  }
 
-  private long countLess(int[] nums, int sum) {
-    long res = 0;
-    for (int i = 0, j = nums.length - 1; i < j; ++i) {
-      while (i < j && nums[i] + nums[j] > sum)
-        --j;
-      res += j - i;
+    long lower_bound(int[] nums, int low, int high, int element) {
+        while (low <= high) {
+            int mid = low + ((high - low) / 2);
+            if (nums[mid] >= element) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
     }
-    return res;
-  }
+
+    long countFairPairs(int[] nums, int lower, int upper) {
+        Arrays.sort(nums);
+        long ans = 0;
+        for (int i = 0; i < nums.length; i++) {
+            // Assume we have picked nums[i] as the first pair element.
+
+            // `low` indicates the number of possible pairs with sum < lower.
+            long low = lower_bound(
+                nums,
+                i + 1,
+                nums.length - 1,
+                lower - nums[i]
+            );
+
+            // `high` indicates the number of possible pairs with sum <= upper.
+            long high = lower_bound(
+                nums,
+                i + 1,
+                nums.length - 1,
+                upper - nums[i] + 1
+            );
+
+            // Their difference gives the number of elements with sum in the
+            // given range.
+            ans += 1 * (high - low);
+        }
+        return ans;
+    }
 }
